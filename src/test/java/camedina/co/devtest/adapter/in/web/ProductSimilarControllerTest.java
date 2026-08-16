d170adf4cf8ae8b1388f44de33dd91b146123366
Camilo Medina <medina1camilo@gmail.com>
2026-08-16 22:08:56 +0200

    [test-solution] add webflux test dependency

--- src/test/java/camedina/co/devtest/adapter/in/web/ProductSimilarControllerTest.java
+++ src/test/java/camedina/co/devtest/adapter/in/web/ProductSimilarControllerTest.java
@@ -4,7 +4,7 @@ import camedina.co.devtest.domain.FindSimilarProducts;
 import camedina.co.devtest.domain.ProductDetail;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.webflux.test.autoconfigure.WebFluxTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
