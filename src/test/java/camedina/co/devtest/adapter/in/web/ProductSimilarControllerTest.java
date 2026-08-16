68f35f67ca13778ba1787a052edce1890a93a8a6
Camilo Medina <medina1camilo@gmail.com>
2026-08-16 22:05:43 +0200

    [test-solution] add ProductSimilarControllerTest.java.

---
+++ src/test/java/camedina/co/devtest/adapter/in/web/ProductSimilarControllerTest.java
@@ -0,0 +1,37 @@
+package camedina.co.devtest.adapter.in.web;
+
+import camedina.co.devtest.domain.FindSimilarProducts;
+import camedina.co.devtest.domain.ProductDetail;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@WebFluxTest(ProductSimilarController.class)
+class ProductSimilarControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockitoBean
+    private FindSimilarProducts findSimilarProducts;
+
+    @Test
+    void returnsSimilarProductsForKnownProduct() {
+        var product2 = new ProductDetail("2", "Product 2", 20.5, true);
+        when(findSimilarProducts.findSimilarProducts("1"))
+                .thenReturn(Flux.just(product2));
+
+        webTestClient.get().uri("/product/1/similar")
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(ProductDetail.class)
+                .isEqualTo(List.of(product2));
+    }
+}
