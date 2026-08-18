f4aaf701846ef4c4075b356f0278a63cd3742536
Camilo Medina <medina1camilo@gmail.com>
2026-08-18 19:26:29 +0200

    [test-solution] remove version from docker-compose. update version. and change from Flux to Mono.

--- src/main/java/camedina/co/devtest/adapter/in/web/ProductSimilarController.java
+++ src/main/java/camedina/co/devtest/adapter/in/web/ProductSimilarController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 class ProductSimilarController {
@@ -21,8 +23,8 @@ class ProductSimilarController {
     }
 
     @GetMapping("/product/{productId}/similar")
-    Flux<ProductDetail> similarProducts(@PathVariable String productId) {
-        return findSimilarProducts.findSimilarProducts(productId);
+    Mono<List<ProductDetail>> similarProducts(@PathVariable String productId) {
+        return findSimilarProducts.findSimilarProducts(productId).collectList();
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
